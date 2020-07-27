54999b3b07890b85348aefa2404025037a9ad17c
Roopkanwal <roopkanwal13@gmail.com>
2020-07-27 17:18:28 -0600

    Add Comments

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,17 @@
+# Ask for user's name
 name=input("What is your name?")
+
+# Ask for user's year of birth
 birth_year=input("When you were born?")
+
+# Greet user with Hello
 print(f"Hello,{name}")
 
+# Calculate and tell age of user
 from datetime import datetime
 this_year = datetime.now().year
 age=this_year-int(birth_year)
 print(f"You must be {age}")
+
+# Say Goodbye to user
 print("Goodbye!")
--- rectangle_summarizer.py
+++ rectangle_summarizer.py
@@ -1,25 +1,15 @@
 import csv
 from statistics import mean
 
+# Open and read the contents of CSV file
 with open("DATA475_lab_rectangles_data.csv",newline="") as f:
     reader = csv.reader(f)
     next(reader)
-    
-    areas=[float(line[1]) * float(line[2]) for line in reader]
-    # for line in reader:        
-    #     # print(line)
-    #     w = float(line[1])
-    #     l = float(line[2])
-    #     area = w * l
-    #     areas.append(area)
-# value=[]
-# value.append(len(areas))
-# value.append(sum(areas))
-# value.append(mean(areas))
-# value.append(max(areas))
-# value.append(min(areas))
-# print(value)
 
+# Calucate the area of rectangles    
+    areas=[float(line[1]) * float(line[2]) for line in reader]
+    
+# Calculates and print the total, average, maximum and minimum area of all rectangles
 column_names = {
     "Total Count" : len(areas),
     "Total Area" : sum(areas),
@@ -31,6 +21,7 @@ column_names = {
 for key,value in column_names.items():
      print(f"{key}: {value}")
 
+# Store the above information in another CSV file named summary.csv
 with open("summary.csv","w",newline="") as f:
     writer = csv.writer(f)
     writer.writerow(column_names.keys())
